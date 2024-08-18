2d0e3fd5da8cb6afc16d55a318f9c1c6b6fa86eb
LynnT-2003 <u6411271@au.edu>
2024-08-18 08:51:01 +0700

    Added service for fetching all developers.

--- services/DeveloperServices.ts
+++ services/DeveloperServices.ts
@@ -1,6 +1,10 @@
 import { client } from "@/sanity/lib/client";
 import { Developer } from "@/types";
 
+export const fetchAllDevelopers = async (): Promise<Developer[]> => {
+  return await client.fetch(`*[_type == "developer"]`);
+};
+
 export const fetchDeveloperById = async (
   id: string
 ): Promise<Developer | null> => {
