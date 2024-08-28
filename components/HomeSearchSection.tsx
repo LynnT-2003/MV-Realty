3925b9bc61df48966876663643cdc34059d4121d
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-08-28 11:11:22 +0700

    Updated search bar functionality: Added default filter function and pause after keyword detection.

--- components/HomeSearchSection.tsx
+++ components/HomeSearchSection.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { PlaceholdersAndVanishInput } from "./ui/placeholders-and-vanish-input";
 import { Listing, Property } from "@/types";
+import { useRouter } from "next/navigation";
 
 interface BrowseCarouselProps {
   listings: Listing[];
@@ -11,15 +13,43 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
   listings,
   properties,
 }) => {
+  const router = useRouter();
+
+  const [filteredListings, setFilteredListings] = useState<Listing[]>([]);
+  const [filteredProperties, setFilteredProperties] = useState<Property[]>([]);
+
+  const handleListingClick = (slug: String) => {
+    router.push(`/ListingDetails/${slug}`);
+  };
+
+  const handlePropertyClick = (slug: String) => {
+    router.push(`/Details/${slug}`);
+  };
+
+  const provinceData = [
+    "Phrom Phong",
+    "Siam",
+    "Asoke",
+    "Sukhumvit",
+    "Phra Khanong",
+    "Thonglor",
+    "Ekkamai",
+    "Silom",
+    "Chatuchak",
+    "Ratchada",
+    "Sathorn",
+  ];
+
   const placeholders = [
-    "Listings around Phra Khanong",
-    "Properties near Chiang Mai",
-    "Close to to BTS",
-    "Property RHYTHM CHaroenkrung Pavillion",
+    "I want to know more info on LIFE Asoke Rama ..",
+    "Details about The Address - 2BR ?",
+    "Listings around Phra Khanong ..",
+    "Properties near Chiang Mai ..",
+    "Any listing close to BTS ?",
   ];
 
   // Define all keywords for different filters
-  const listingPropertyKeywords = ["on", "about", "for", "in"];
+  const listingPropertyKeywords = [" on", " about", " for", " in"];
 
   const addressInfoKeywords = [
     "close to",
@@ -33,6 +63,23 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
   // Function to determine if a value contains any keywords from the provided list
   const containsKeywords = (value: string, keywords: string[]): boolean => {
     return keywords.some((keyword) => value.toLowerCase().includes(keyword));
+  };
+
+  // Define default filter function (Get and Match listing + property names)
+  const filterDefaultNameTitle = (
+    value: string,
+    listings: Listing[],
+    properties: Property[]
+  ): { filteredListings: Listing[]; filteredProperties: Property[] } => {
+    const filteredListings = listings.filter((listing) =>
+      listing.listingName.toLowerCase().includes(value.toLowerCase())
+    );
+
+    const filteredProperties = properties.filter((property) =>
+      property.title.toLowerCase().includes(value.toLowerCase())
+    );
+
+    return { filteredListings, filteredProperties };
   };
 
   // Define filter function for listing and property names
@@ -53,6 +100,10 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
         cleanedValue = cleanedValue.slice(keywordIndex + keyword.length).trim();
         break; // Exit the loop once a keyword is found and cleaned
       }
+    }
+
+    if (cleanedValue.length === 0) {
+      return { filteredListings: [], filteredProperties: [] };
     }
 
     const filteredListings = listings.filter((listing) =>
@@ -94,6 +145,10 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
       property.description.toLowerCase().includes(cleanedValue.toLowerCase())
     );
 
+    if (cleanedValue.length === 0) {
+      return { filteredListings: [], filteredProperties: [] };
+    }
+
     return { filteredListings, filteredProperties };
   };
 
@@ -126,11 +181,12 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
       const result = filterByListingPropertyNames(value, listings, properties);
       filteredListings = result.filteredListings;
       filteredProperties = result.filteredProperties;
-      const resultFromAddressInfoFilter = filterByAddressInfo(
-        value,
-        listings,
-        properties
-      );
+    }
+
+    if (!isListingSearch && !isAddressSearch && value != "") {
+      const result = filterDefaultNameTitle(value, listings, properties);
+      filteredListings = result.filteredListings;
+      filteredProperties = result.filteredProperties;
     }
 
     return { filteredListings, filteredProperties };
@@ -147,6 +203,10 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
       listings,
       properties
     );
+
+    // Update state with filtered results
+    setFilteredListings(filteredListings);
+    setFilteredProperties(filteredProperties);
 
     console.log(
       "Filtered Listings: ",
@@ -174,6 +234,41 @@ const HomeSearchSection: React.FC<BrowseCarouselProps> = ({
         onChange={handleChange}
         onSubmit={onSubmit}
       />
+
+      <div className="mt-2 w-full relative max-w-xl mx-auto max-h-[0px]">
+        <ul className="rounded-3xl shadow-lg max-h-[160px] overflow-scroll">
+          {filteredListings.map((listing) => (
+            <div
+              className="bg-gray-100 hover:bg-gray-300 hover:cursor-pointer pl-8 py-2 text-gray-600 hover:text-black"
+              onClick={() => {
+                handleListingClick(listing._id);
+              }}
+            >
+              <span key={listing._id}>
+                <span className="text-gray-500 text-xs items-center pr-3">
+                  <i>Listing:</i>
+                </span>
+                {listing.listingName}
+              </span>
+            </div>
+          ))}
+          {filteredProperties.map((property) => (
+            <div
+              className="bg-gray-100 hover:bg-gray-300 hover:cursor-pointer pl-8 py-2 text-gray-600 hover:text-black"
+              onClick={() => {
+                handlePropertyClick(property.slug.current);
+              }}
+            >
+              <span key={property._id}>
+                <span className="text-gray-500 text-xs items-center pr-3">
+                  <i>Property:</i>
+                </span>
+                {property.title}
+              </span>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
