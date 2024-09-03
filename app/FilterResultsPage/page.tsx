4c27511690a67b971fcbc6d2620ef61f2684cf5f
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-09-03 12:52:14 +0700

    Completed Filter Search Reaults Page using ListingCardCollection component.

--- app/FilterResultsPage/page.tsx
+++ app/FilterResultsPage/page.tsx
@@ -1,9 +1,75 @@
 "use client";
 
+import BrowseCarousel from "@/components/BrowseCarouselListing";
+import ListingCardCollection from "@/components/ListingCardCollection";
+import { Property, Listing, Developer } from "@/types";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const FilterResultsPage: React.FC = () => {
   const searchParams = useSearchParams();
+
+  const [properties, setProperties] = useState<Property[]>([]);
+  const [listings, setListings] = useState<Listing[]>([]);
+  const [developers, setDevelopers] = useState<Developer[]>([]);
+
+  const getLocalStorage = (key: string) => {
+    const storedData = localStorage.getItem(key);
+    return storedData ? JSON.parse(storedData) : null;
+  };
+
+  const fetchDataWithCache = async (key: string) => {
+    const storedData = getLocalStorage(key);
+    if (storedData) {
+      return storedData;
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch data in parallel, using cached data if available
+        const [propertiesData, listingsData, developersData] =
+          await Promise.all([
+            fetchDataWithCache("properties"),
+            fetchDataWithCache("listings"),
+            fetchDataWithCache("developers"),
+          ]);
+
+        const filteredBedroom = listingsData.filter((listing: Listing) => {
+          return bedrooms ? listing.bedroom === Number(bedrooms) : true;
+        });
+
+        const filteredMinPrice = filteredBedroom.filter((listing: Listing) => {
+          // Check if minprice is provided and greater than 0
+          return minprice && Number(minprice) > 0
+            ? listing.price >= Number(minprice)
+            : true;
+        });
+
+        const filteredMaxPrice = filteredMinPrice.filter((listing: Listing) => {
+          // Check if maxprice is provided and greater than 0
+          return maxprice && Number(maxprice) > 0
+            ? listing.price <= Number(maxprice)
+            : true;
+        });
+
+        const filteredLocation = filteredMaxPrice.filter((listing: Listing) => {
+          return location ? listing.listingName.includes(location) : true;
+        });
+
+        const FilteredListings = filteredLocation;
+
+        setProperties(propertiesData);
+        setListings(FilteredListings);
+        setDevelopers(developersData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   // Retrieve query parameters
   const bedrooms = searchParams.get("bedrooms") || "";
@@ -19,7 +85,15 @@ const FilterResultsPage: React.FC = () => {
   console.log("Location:", location);
   console.log("Buy/Rent:", buyRent);
 
-  return <div>FilterResultsPage</div>;
+  return (
+    <div className="pt-12">
+      <ListingCardCollection
+        listings={listings}
+        properties={properties}
+        developers={developers}
+      />
+    </div>
+  );
 };
 
 export default FilterResultsPage;
---
+++ components/ListingCardCollection.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { urlForImage } from "@/sanity/lib/image";
+import { Listing, Property, Developer } from "@/types";
+import { DirectionAwareHover } from "./ui/direction-aware-hover";
+
+interface ListingCardCollectionProps {
+  listings: Listing[];
+  properties: Property[];
+  developers: Developer[];
+}
+
+const ListingCardCollection: React.FC<ListingCardCollectionProps> = ({
+  listings,
+  properties,
+  developers,
+}) => {
+  return (
+    <div className="w-full flex justify-center pt-3 pb-12 md:pb-20">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {listings.map((listing, index) => {
+          const property =
+            properties.find((prop) => prop._id === listing.property._ref) ||
+            properties[0];
+
+          const developer =
+            developers.find((dev) => dev._id === property.developer._ref) ||
+            developers[0];
+
+          return (
+            <div
+              key={index}
+              onClick={() => {
+                // handlePropertyClick(listing._id);
+              }}
+              className="relative inline-block mb-4 md:mb-0 group"
+            >
+              <DirectionAwareHover imageUrl={urlForImage(listing.listingHero)}>
+                <span className="font-semibold text-sm">
+                  Price: {listing.price}M Baht
+                </span>
+              </DirectionAwareHover>
+              <div className="mt-2 p-2 bg-white rounded-lg mb-8">
+                <span className="flex items-center text-lg font-semibold">
+                  {listing.listingName}
+                  <img
+                    src={urlForImage(developer.profileIcon)}
+                    className="w-8 h-8 ml-1"
+                  />
+                </span>
+                {/* <p className="font-normal text-sm">$1299 / night</p> */}
+                <div className="flex pt-2">
+                  <span className="pr-6 flex">
+                    <img src="/icons/bedroom.png" className="pr-2" />
+                    {listing.bedroom}
+                  </span>
+                  <span className="pr-6 flex">
+                    <img src="/icons/meter.png" className="pr-2" />
+                    {listing.size}
+                  </span>
+                  <span className="pr-6 flex">
+                    <img src="/icons/shower.png" className="pr-2" />
+                    {listing.bathroom}
+                  </span>
+                  <span className="pr-6 flex">
+                    <img src="/icons/floor.png" className="pr-2" />
+                    {listing.floor}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ListingCardCollection;
