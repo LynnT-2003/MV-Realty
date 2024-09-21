ba9d9edc42120615afc05849a8062746f04e0891
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-09-22 05:55:06 +0700

    Undo comment mistake

--- components/PropertyDetailsIntro.tsx
+++ components/PropertyDetailsIntro.tsx
@@ -56,15 +56,12 @@ const PropertyDetailsIntro: React.FC<PropertyDetailsIntroProps> = ({
       <div className="md:max-w-[1150px] w-[85vw]">
         <Grid container columnSpacing={10}>
           <Grid item md={7}>
-            // Title of the property
             <p className="poppins-text-title-small md:property-details-title-text">
               {propertyDetails.title}
             </p>
-            // Description of the property
             <p className="poppins-text-small md:poppins-text-avg mt-4">
               {propertyDetails.description}
             </p>
-            // Tags for the property
             <div className="flex flex-wrap gap-2 mt-5">
               {tags.map((tag, index) => (
                 <button
@@ -78,7 +75,6 @@ const PropertyDetailsIntro: React.FC<PropertyDetailsIntroProps> = ({
             </div>
           </Grid>
           <Grid item md={5}>
-            // Grid of information about the property
             <Grid
               container
               rowSpacing={{ xs: 4, md: 3 }}
@@ -86,14 +82,12 @@ const PropertyDetailsIntro: React.FC<PropertyDetailsIntroProps> = ({
               //   spacing={{ xs: 0, md: 2 }}
               spacing={2}
             >
-              // Price of the property
               <div className="flex w-full ml-3.5 mb-8 mt-8">
                 <img src="/icons/PriceTag.svg" className="w-10 h-10" />
                 <p className="ml-3.5 pt-1 text-[#193158] text-2xl font-semibold">
                   Price Starts from {propertyDetails.minPrice}MB
                 </p>
               </div>
-              // Completed on date
               <Grid item xs={6}>
                 <div className="flex">
                   <img src="/icons/Calendar.svg" />
@@ -102,7 +96,6 @@ const PropertyDetailsIntro: React.FC<PropertyDetailsIntroProps> = ({
                   </p>
                 </div>
               </Grid>
-              // Developer of the property
               <Grid item xs={6}>
                 <div className="flex">
                   <img src="/icons/Developer.svg" />
@@ -112,7 +105,7 @@ const PropertyDetailsIntro: React.FC<PropertyDetailsIntroProps> = ({
                 </div>
               </Grid>
             </Grid>
-            // Buttons to share the property and contact us
+
             <div className="flex justify-between gap-4 mt-20">
               <button className="py-2 bg-[#193158] text-white font-semibold rounded-lg w-1/2 text-xs">
                 SHARE THIS PROPERTY
@@ -121,23 +114,21 @@ const PropertyDetailsIntro: React.FC<PropertyDetailsIntroProps> = ({
                 CONTACT US
               </button>
             </div>
-            // Button to view listings for this property
+
             <div className="mt-4">
               <button
                 className="py-2 bg-[#193158] text-white font-semibold rounded-lg w-full text-xs"
                 onClick={() => {
-                  // Encode the listings as a JSON string
                   const listingsParam = encodeURIComponent(
                     JSON.stringify(listings)
                   );
 
-                  // Navigate to the listings page and pass the listings as a parameter
                   router.push(`/Listings?listings=${listingsParam}`);
                 }}
               >
                 LISTINGS FROM THIS PROPERTY
               </button>
-              // Property ID
+
               <p className="mt-4 text-[#193158] font-bold text-center text-lg">
                 Property ID: {propertyDetails._id}
               </p>
